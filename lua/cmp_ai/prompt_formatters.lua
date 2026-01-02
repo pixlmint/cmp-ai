96ed38223dac0198631587c9fc5c066315ff3074
Christian Gröber <swiss8oy.chg@gmail.com>
2026-01-02 18:26:18 +0100

    feat: improve prompt formatters

--- lua/cmp_ai/prompt_formatters.lua
+++ lua/cmp_ai/prompt_formatters.lua
@@ -1,43 +1,65 @@
 local M = {}
 
+
+local function chat_formatting(user_msgs, system_msg)
+  -- TODO: Add support to also include old assistant messages
+  local chat = {}
+  if system_msg ~= nil then
+    table.insert(chat, {
+      role = "system",
+      content = system_msg
+    })
+  end
+  if type(user_msgs) == 'string' then
+    user_msgs = {user_msgs}
+  end
+  for _, msg in pairs(user_msgs) do
+    table.insert(chat, {
+      role = "user",
+      content = msg,
+    })
+  end
+
+  return {
+    prompt = vim.fn.json_encode(chat),
+  }
+end
+
 -- Table of prompt formatters for different providers
 M.formatters = {
   -- for general chat models (gpt/claude)
-  general_ai = {
-    system = function(filetype)
-      return [=[You are a coding companion.
-You need to suggest code for the language ]=] .. filetype .. [=[
+  general_ai = function(lines_before, lines_after, opts)
+    opts = opts or {filetype = vim.bo.filetype}
+    local user_message = string.format("<code_prefix>%s</code_prefix><code_suffix>%s</code_suffix><code_middle>", lines_before, lines_after)
+    local system = [=[You are a coding companion.
+You need to suggest code for the language ]=] .. opts.filetype .. [=[
 
 Given some code prefix and suffix for context, output code which should follow the prefix code.
-You should only output valid code in the language ]=] .. filetype .. [=[
+You should only output valid code in the language ]=] .. opts.filetype .. [=[
 . to clearly define a code block, including white space, we will wrap the code block
 with tags.
 Make sure to respect the white space and indentation rules of the language.
 Do not output anything in plain language, make sure you only use the relevant programming language verbatim.
 For example, consider the following request:
-<begin_code_prefix>def print_hello():<end_code_prefix><begin_code_suffix>\n    return<end_code_suffix><begin_code_middle>
+<code_prefix>def print_hello():</code_prefix><code_suffix>\n    return</code_suffix><code_middle>
 Your answer should be:
 
-    print("Hello")<end_code_middle>
+    print("Hello")</code_middle>
 ]=]
-    end,
-    user = function(lines_before, lines_after)
-      return '<begin_code_prefix>' .. lines_before .. '<end_code_prefix>'
-          .. '<begin_code_suffix>' .. lines_after .. '<end_code_suffix><begin_code_middle>'
-    end,
-  },
+    return chat_formatting(user_message, system)
+end,
 
   -- Ollama FIM format (no system prompt, uses special tokens)
   ollama_code = function(lines_before, lines_after)
-    return '<PRE> ' .. lines_before .. ' <SUF>' .. lines_after .. ' <MID>'
+    return chat_formatting('<PRE> ' .. lines_before .. ' <SUF>' .. lines_after .. ' <MID>')
   end,
 
   santacoder = function(lines_before, lines_after)
-    return '<fim-prefix>' .. lines_before .. '<fim-suffix>' .. lines_after .. '<fim-middle>'
+    return chat_formatting('<fim-prefix>' .. lines_before .. '<fim-suffix>' .. lines_after .. '<fim-middle>')
   end,
 
   codestral = function(lines_before, lines_after)
-    return '[SUFFIX]' .. lines_before .. '[PREFIX]' .. lines_after
+    return chat_formatting('[SUFFIX]' .. lines_before .. '[PREFIX]' .. lines_after)
   end,
 
   -- used for codegemma and qwen
